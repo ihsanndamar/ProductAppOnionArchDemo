e17d26e786e248fb142aa6c1f372c2ca594df6e9
ihsanndamar <111857789+ihsanndamar@users.noreply.github.com>
2022-10-25 23:12:53 +0300

    feat: added service registrations

---
+++ src/Infrastructure/ProductApp.Persistance/ServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProductApp.Application.Interfaces.Repository;
+using ProductApp.Persistance.Context;
+using ProductApp.Persistance.Repositories;
+
+namespace ProductApp.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceService(this IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(opt => opt.UseInMemoryDatabase("memoryDb"));
+            services.AddTransient<IProductRepository, ProductRepository>();
+
+        } 
+    }
+}
