36be36155319353d6565e1b5bd7a47f4568c4275
ihsanndamar <111857789+ihsanndamar@users.noreply.github.com>
2022-10-25 18:20:43 +0300

    feat: create app api layer

---
+++ src/WebApi/ProductApp.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Application.Interfaces.Repository;
+
+namespace ProductApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+
+        [HttpGet]
+        public Task<IActionResult> Get()
+        {
+            return Task.FromResult<IActionResult>(Ok());
+        }
+    }
+}
